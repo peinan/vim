a01f74886070fbf913bdb533ea976c9e4332535e
Peinan ZHANG <peinan7@gmail.com>
2017-04-24 12:46:16 +0000

    :pencil2: updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-cp -r .vim .vimrc $HOME
+# path of dein dir
+dein_dir="$HOME/.vim/dein"
+
+# sync to local exclude .vim/dein
+rsync -a .vim $HOME --exclude '.vim/dein'
+
+# if no dein dir, then make it
+if [ ! -e $dein_dir ]; then
+  mkdir -p $dein_dir
+fi
