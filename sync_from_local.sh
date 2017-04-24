c8c89ffb9fd84dc902d3260dfc2e1d822f4f59c8
Peinan ZHANG <peinan7@gmail.com>
2017-04-24 12:50:00 +0000

    :pencil2: updated sync_from_local.sh

--- sync_from_local.sh
+++ sync_from_local.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-cp -r ${HOME}/.vim .
+# sync from local exclude .vim/dein
+rsync -a ${HOME}/.vim . --exclude "${HOME}/.vim/dein"
+
+# copy .vimrc from local
 cp ${HOME}/.vimrc .
+
+# if no dein dir here, then make it
+if [ ! -e ".vim/dein" ]; then
+  mkdir -p .vim/dein
+  touch .vim/dein/.gitkeep
+fi
