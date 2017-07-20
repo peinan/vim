680346eb3d30e4874a6f0d3a1f547807b8f54387
peinan <peinan7@gmail.com>
2017-07-20 16:34:46 +0900

    modified to copy .vimrc

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ dein_dir="$HOME/.vim/dein"
 # sync to local exclude .vim/dein
 rsync -a .vim $HOME --exclude '.vim/dein'
 
+cp ./.vimrc $HOME
+
 # if no dein dir, then make it
 if [ ! -e $dein_dir ]; then
   mkdir -p $dein_dir
